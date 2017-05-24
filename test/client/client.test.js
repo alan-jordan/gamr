3bb99c00ecd9f30ac4efcb175fb1f1a31b1bb7bf
Alan Jordan <alanpjordan@gmail.com>
2017-05-24 16:09:42 +1200

    Enzyme and Ava set up. Two tests passing

---
+++ test/client/client.test.js
@@ -0,0 +1,18 @@
+import test from 'ava'
+import React from 'react'
+import { shallow, mount, render } from 'enzyme'
+
+import './setup-dom'
+import App from '../../client/components/App'
+
+App.prototype.componentDidMount = () => {}
+
+test('Container class exists', t => {
+  const wrapper = mount(<App />)
+  t.is(wrapper.find('.container').exists(), true)
+})
+
+test('Logo class in nav bar contains gamr', t => {
+  const wrapper = mount(<App />)
+  t.is(wrapper.find('.logo a').text(), 'gamr')
+})
