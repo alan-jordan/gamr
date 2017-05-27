7fae342e8f215284c742500d1bd387f2043f62b6
Alan Jordan <alanpjordan@gmail.com>
2017-05-27 18:10:55 +1200

    Starting library feature again

--- client/components/Library.jsx
+++ client/components/Library.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 import * as api from '../api'
-import LibraryRender from './LibraryRender'
 
 
 export default class Library extends React.Component {
@@ -32,22 +31,7 @@ export default class Library extends React.Component {
       })
     }
 
-    renderGames() {
-      return this.state.userGames.games.map((game) => {
-        return (
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-           <div className="thumbnail" >
-             <a href={`/game/${game.game_id}`}>
-             <img src={gam.game_box_art} className="img-responsive" alt={game.game_name} />
-             <p>{game.game_name}</p>
-             </a>
-             <a href="/user">Edit status</a>
-            </div>
-          </div>
-        )
-      })
 
-    }
 
   render() {
     return (
@@ -57,7 +41,7 @@ export default class Library extends React.Component {
         </div>
         <div className="col-md-10">
           <div className="libraryHeader">
-            {this.renderGames()}
+
           </div>
       </div>
     </div>
--- public/bundle.js
+++ public/bundle.js
@@ -11551,10 +11551,6 @@ var _api = __webpack_require__(25);
 
 var api = _interopRequireWildcard(_api);
 
-var _LibraryRender = __webpack_require__(237);
-
-var _LibraryRender2 = _interopRequireDefault(_LibraryRender);
-
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -11606,35 +11602,6 @@ var Library = function (_React$Component) {
       });
     }
   }, {
-    key: 'renderGames',
-    value: function renderGames() {
-      return this.state.userGames.games.map(function (game) {
-        return _react2.default.createElement(
-          'div',
-          { className: 'col-lg-3 col-md-4 col-xs-6 thumb' },
-          _react2.default.createElement(
-            'div',
-            { className: 'thumbnail' },
-            _react2.default.createElement(
-              'a',
-              { href: '/game/' + game.game_id },
-              _react2.default.createElement('img', { src: gam.game_box_art, className: 'img-responsive', alt: game.game_name }),
-              _react2.default.createElement(
-                'p',
-                null,
-                game.game_name
-              )
-            ),
-            _react2.default.createElement(
-              'a',
-              { href: '/user' },
-              'Edit status'
-            )
-          )
-        );
-      });
-    }
-  }, {
     key: 'render',
     value: function render() {
       return _react2.default.createElement(
@@ -11648,11 +11615,7 @@ var Library = function (_React$Component) {
         _react2.default.createElement(
           'div',
           { className: 'col-md-10' },
-          _react2.default.createElement(
-            'div',
-            { className: 'libraryHeader' },
-            this.renderGames()
-          )
+          _react2.default.createElement('div', { className: 'libraryHeader' })
         )
       );
     }
@@ -28071,90 +28034,6 @@ var valueEqual = function valueEqual(a, b) {
 };
 
 exports.default = valueEqual;
-
-/***/ }),
-/* 237 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = __webpack_require__(6);
-
-var _react2 = _interopRequireDefault(_react);
-
-var _reactRouterDom = __webpack_require__(23);
-
-var _api = __webpack_require__(25);
-
-var api = _interopRequireWildcard(_api);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var LibraryRender = function (_React$Component) {
-  _inherits(LibraryRender, _React$Component);
-
-  function LibraryRender(props) {
-    _classCallCheck(this, LibraryRender);
-
-    var _this = _possibleConstructorReturn(this, (LibraryRender.__proto__ || Object.getPrototypeOf(LibraryRender)).call(this, props));
-
-    _this.state = {
-      game: _this.props.game
-    };
-    return _this;
-  }
-
-  _createClass(LibraryRender, [{
-    key: 'componentDidMount',
-    value: function componentDidMount() {}
-  }, {
-    key: 'render',
-    value: function render() {
-      return _react2.default.createElement(
-        'div',
-        { className: 'col-lg-3 col-md-4 col-xs-6 thumb' },
-        _react2.default.createElement(
-          'div',
-          { className: 'thumbnail' },
-          _react2.default.createElement(
-            'a',
-            { href: '/game/' + this.game.game_id },
-            _react2.default.createElement('img', { src: this.game.game_box_art, className: 'img-responsive', alt: this.game.game_name }),
-            _react2.default.createElement(
-              'p',
-              null,
-              this.game.game_name
-            )
-          ),
-          _react2.default.createElement(
-            'a',
-            { href: '/user' },
-            'Edit status'
-          )
-        )
-      );
-    }
-  }]);
-
-  return LibraryRender;
-}(_react2.default.Component);
-
-exports.default = LibraryRender;
 
 /***/ })
 /******/ ]);
